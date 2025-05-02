56b48676f4dfd7eacb68cb978c86bc0673dbc64e
mdfleming3 <38577969+mdfleming3@users.noreply.github.com>
2025-05-02 18:50:40 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,11 @@ document.querySelectorAll('.affiliate-link').forEach(link => {
     // You could integrate Google Analytics event tracking here
   });
 });
+
+async function loadPartial(id, file) {
+  const res = await fetch(file);
+  const html = await res.text();
+  document.getElementById(id).innerHTML = html;
+}
+loadPartial("header", "/partials/header.html");
+loadPartial("footer", "/partials/footer.html");
